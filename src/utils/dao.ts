b83af7e6c356d7c651e50530c7808b181a1c5af5
great-park <cksgh1735@naver.com>
2022-11-10 23:10:51 +0900

    feat: add Query

--- src/utils/dao.ts
+++ src/utils/dao.ts
@@ -27,43 +27,45 @@ export async function userPerCategorySet() {
     }
 }
 
-// //임의 작성, 동작x
-// export async function getCategoryOnToday() {
-//     const connection = await pool.getConnection();
-//     try {
-//         const query: string =
-//             `
-//             selct categoryName from Category where createdAt = currentTimeStamp();
-//         `;
+//임의 작성, 동작x
+export async function getCategoryOnToday(): Promise<string[]> {
+    const connection = await pool.getConnection();
+    try {
+        const query: string =
+            `
+            select distinct categoryName from Notice inner join Category C on Notice.categoryId = C.categoryId
+            where date = current_date();;
+        `;
 
-//         const row = await connection.query(query);
-//         return row[0];
+        const row = await connection.query(query);
+        return Object.values(JSON.parse(JSON.stringify(row[0])));
 
-//     } catch (error) {
-//         logger.error(`getCategoryOnToday Error, message :`, { message: error.toString() })
-//         return null;
-//     } finally {
-//         await connection.release();
-//     }
-// }
+    } catch (error) {
+        logger.error(`getCategoryOnToday Error, message :`, { message: error.toString() })
+        return null;
+    } finally {
+        await connection.release();
+    }
+}
 
 
-// //임의 작성, 동작x
-// export async function getContent(category : string) {
-//     const connection = await pool.getConnection();
-//     try {
-//         const query: string =
-//             `
-//             selct x,x,x,x,x,x, from XX where categoryName = ${category};
-//         `;
+export async function getNotices(category: string): Promise<Object[]> {
+    const connection = await pool.getConnection();
+    try {
+        const query: string =
+            `
+            select title, content, writer, date, provider from Notice inner join Category C on Notice.categoryId = C.categoryId 
+            where categoryName =  '${category}' and date = current_date();
+        `;
 
-//         const row = await connection.query(query);
-//         return row[0];
+        const row = await connection.query(query);
 
-//     } catch (error) {
-//         logger.error(`getContent Error, message :`, { message: error.toString() })
-//         return null;
-//     } finally {
-//         await connection.release();
-//     }
-// }
+        return Object.values(JSON.parse(JSON.stringify(row[0])));
+
+    } catch (error) {
+        logger.error(`getContent Error, message :`, { message: error.toString() })
+        return null;
+    } finally {
+        await connection.release();
+    }
+}
